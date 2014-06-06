429d531abec1cf921707407d0e62e91b8e2f6093
mmcdon20 <mmcdon20@live.com>
2014-06-06 14:57:36 -0500

    added addition checks for test cases that extract zip files with folders

--- src/test/scala/com/github/zipper/ConverterIntegrationTest.scala
+++ src/test/scala/com/github/zipper/ConverterIntegrationTest.scala
@@ -16,24 +16,32 @@ class ConverterIntegrationTest extends Specification with ZipperData with ZipDat
       val name = out + "zip then unzip\\zip.zip"
       val zip = filesWithDirectory.makeZip(name)
       val unzip = zip.extractZip(out + "zip then unzip")
+
       zip.getName === name
       zip.getEntry("a.txt").getName === "a.txt"
       zip.getEntry("dir/d.txt").getName === "dir/d.txt"
       zip.getEntry("dir/dir2/e.txt").getName === "dir/dir2/e.txt"
+
       unzip.size === 4
       unzip.exists(_.getName == "dir") === true
+
+      unzip.find(_.getName == "dir").get.listFiles.exists(_.getName == "d.txt") === true
     }
 
     "Be able to unzip and then zip" in {
       val name = out + "unzip then zip\\zip.zip"
       val unzip = zip2.extractZip(out + "unzip then zip")
       val zip = unzip.makeZip(name)
+
       zip.getName === name
       zip.getEntry("a.txt").getName === "a.txt"
       zip.getEntry("dir/d.txt").getName === "dir/d.txt"
       zip.getEntry("dir/dir2/e.txt").getName === "dir/dir2/e.txt"
+
       unzip.size === 4
       unzip.exists(_.getName == "dir") === true
+
+      unzip.find(_.getName == "dir").get.listFiles.exists(_.getName == "d.txt") === true
     }
 
     "Be able to unzip and then zip and then unzip" in {
@@ -41,14 +49,19 @@ class ConverterIntegrationTest extends Specification with ZipperData with ZipDat
       val unzip = zip2.extractZip(out + "unzip then zip then unzip")
       val zip = unzip.makeZip(name)
       val unzip2 = zip.extractZip(out + "unzip then zip then unzip\\unzip2")
+
       zip.getName === name
       zip.getEntry("a.txt").getName === "a.txt"
       zip.getEntry("dir/d.txt").getName === "dir/d.txt"
       zip.getEntry("dir/dir2/e.txt").getName === "dir/dir2/e.txt"
+
       unzip.size === 4
       unzip.exists(_.getName == "dir") === true
       unzip2.size === 4
       unzip2.exists(_.getName == "dir") === true
+
+      unzip.find(_.getName == "dir").get.listFiles.exists(_.getName == "d.txt") === true
+      unzip2.find(_.getName == "dir").get.listFiles.exists(_.getName == "d.txt") === true
     }
   }
 }
